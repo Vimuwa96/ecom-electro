9b2eadcf4e324bf195223eba7f0a6f672ed9f7fe
Vimukthi Malinda <114574555+Vimuwa96@users.noreply.github.com>
2024-05-06 13:06:19 +0530

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $(".your-class").slick({
+      autoplay: true,
+    });
+  });
+
+
+  $(document).ready(function () {
+    $(".brands-wrapper").slick({
+      autoplay: true,
+      slidesToShow: 5,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  });
